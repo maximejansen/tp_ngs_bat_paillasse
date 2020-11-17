784f1650377e485c8422c63a61dadb37810ecf96
Maxime JANSEN <maxime.jansen@ens-lyon.fr>
2020-11-17 15:27:14 +0000

    ajout de la réalisation de Trimmomatic pour enlever les mauvaises séquences et mauvais rides.

--- Trinity.sh
+++ Trinity.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
+# Dossier par défaut. 
+data="data/mydatalocal"
+cd
+cd $data
+
+#création du dossier pour FASTQC. 
+mkdir -p mydata/fastqc
+
+#Récupérer mes données fastq:
+fasta="download/Projet_31_20_UE_NGS_2020/FASTQ/*.gz"
+
 #utilisation de Trinity test avec le premier jeu de données
 
 
@@ -21,18 +32,110 @@ Trinity --seqType fq \
 
 # test de la boucle FASTQC: elle fonctionne correctement. 
 
-for fichier in download/sharegate-igfl.ens-lyon.fr/Projet_31_20_UE_NGS_2020/FASTQ/*.gz
+for fichier in $fasta
 do
   echo "$fichier"
 done
 
 # realisation et collecter des données en local. 
 
-for fichier in download/sharegate-igfl.ens-lyon.fr/Projet_31_20_UE_NGS_2020/FASTQ/*.gz
+for fichier in $fasta
 do
-  fastqc $fichier --outdir download/fastqc \
+  fastqc $fichier --outdir mydata/fastqc \
         --format fastq \
         --threads 8
 done
+
+#j'utilise Trimmomatic pour retirer les mauvais rides des fichier
+#création des output Trimmomatic
+mkdir mydata/trimmomatic
+
+#variavle pour input et output 
+input="download/Projet_31_20_UE_NGS_2020"
+output="mydata/trimmomatic"
+
+# Utilisation de Trimmomatic:
+
+java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar -threads 8 -#! /bin/bash
+
+# Dossier par défaut. 
+data="data/mydatalocal"
+cd
+cd $data
+
+#création du dossier pour FASTQC. 
+mkdir -p mydata/fastqc
+
+#Récupérer mes données fastq:
+fasta="download/Projet_31_20_UE_NGS_2020/FASTQ/*.gz"
+
+#utilisation de Trinity test avec le premier jeu de données
+
+
+Trinity --seqType fq \
+        --max_memory 10G \
+        --CPU 4 \
+        --left Lib1_31_20_S1_R1_001.fastq.gz \
+        --right Lib1_31_20_S1_R2_001.fastq.gz
+
+#Résultat: cela prend une heure juste pour un jeu de données.
+        
+#Utinisation generalisé de trinity
+        
+Trinity --seqType fq \
+        --max_memory 14G \
+        --CPU 4 \
+        --left Lib1_31_20_S1_R1_001.fastq.gz, \
+        --right Lib4_31_20_S4_R2_001.fastq.gz,
+
+# test de la boucle FASTQC: elle fonctionne correctement. 
+
+for fichier in $fasta
+do
+  echo "$fichier"
+done
+
+# realisation et collecter des données en local. 
+
+for fichier in $fasta
+do
+  fastqc $fichier --outdir mydata/fastqc \
+        --format fastq \
+        --threads 8
+done
+
+#j'utilise Trimmomatic pour retirer les mauvais rides des fichier
+#création des output Trimmomatic
+mkdir mydata/trimmomatic
+
+#variavle pour input et output 
+
+input="download/Projet_31_20_UE_NGS_2020/FASTQ"
+output="mydata/trimmomatic"
+
+#variable fasta et nombre pour la boucle ainsi le nom des fichiers 
+
+fasta="download/adapt.fasta"
+boucle="1
+2
+3
+4
+5
+6"
+# Utilisation de Trimmomatic:
+#test de la boucle
+for n in $boucle
+do
+  echo "${n} fois"
+done
+
+#vrai essay: ça télécharge bien sur mydata/trimmomatic
+for n in $boucle
+do
+  java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 8 -phred33 $input/Lib${n}_31_20_S${n}_R1_001.fastq.gz $input/Lib${n}_31_20_S${n}_R2_001.fastq.gz $output/Lib${n}_31_20_S${n}_R1_paired.fastq.gz $output/Lib${n}_31_20_S${n}_R1_unpaired.fastq.gz $output/Lib${n}_31_20_S${n}_R2_paired.fastq.gz $output/Lib${n}_31_20_S${n}_R2_unpaired.fastq.gz ILLUMINACLIP:${fasta}:2:30:10 HEADCROP:9 MINLEN:100
+done
+
+
+
 
 
