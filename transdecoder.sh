7f1e764d7b47dd8bd9feb63c80ae9a86abaed142
Maxime JANSEN <maxime.jansen@ens-lyon.fr>
2020-11-19 13:22:05 +0000

    J'ai enlevé ce qui n'avait pas à faire là.

--- transdecoder.sh
+++ transdecoder.sh
@@ -4,10 +4,3 @@
 data="data/mydatalocal"
 cd
 cd $data
-
-
-grep ">" mydata/trinity/Trinity_RF.fasta |less
-grep ">" mydata/trinity/Trinity_RF.fasta |cut -f1,2,3,4 -d "_"|less
-grep ">" mydata/trinity/Trinity_RF.fasta |cut -f1,2,3,4 -d "_"|short |less
-grep ">" mydata/trinity/Trinity_RF.fasta |cut -f1,2,3,4 -d "_"|short |uniq |less
-grep ">" mydata/trinity/Trinity_RF.fasta |cut -f1,2,3,4 -d "_"|short |uniq |wc -l
