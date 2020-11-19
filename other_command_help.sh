325428ef18c2d9d4195a5ed80c3d3bbced7ee528
Maxime JANSEN <maxime.jansen@ens-lyon.fr>
2020-11-19 13:21:32 +0000

    Fichier comprenant des codes autres qui me sont utiles

---
+++ other_command_help.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Dossier par défaut. 
+data="data/mydatalocal"
+cd
+cd $data
+
+
+grep ">" mydata/trinity/Trinity_RF.fasta |less
+grep ">" mydata/trinity/Trinity_RF.fasta |cut -f1,2,3,4 -d "_"|less
+grep ">" mydata/trinity/Trinity_RF.fasta |cut -f1,2,3,4 -d "_"|sort|less
+grep ">" mydata/trinity/Trinity_RF.fasta |cut -f1,2,3,4 -d "_"|sort |uniq |less
+grep ">" mydata/trinity/Trinity_RF.fasta |cut -f1,2,3,4 -d "_"|sort |uniq |wc -l
